f1995044697d2940a85e6d583446ddc923e721f0
GONGPARK <79816486+kanggongp@users.noreply.github.com>
2024-05-07 22:30:21 +0900

    Develop (#2)
    * click 01
    
    * click 02
    
    * click 03

--- src/router/Home/HomeContainer.js
+++ src/router/Home/HomeContainer.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 function HomeContainer(props) {
+
+  const click = () => {
+    // console.log('clilck test1');
+    // console.log('clilck test2');
+    console.log('clilck test3');
+    console.log('clilck test4');
+
+    console.log('clilck test11');
+    console.log('clilck test21');
+    console.log('clilck test31');
+    console.log('clilck test41');
+
+  }
+
   return (
     <div>
       HomeContainer
+      <div>
+        test
+      </div>
+      <button onClick={click}>click</button>
     </div>
   );
 }
