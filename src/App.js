e4231f7abc21693be000a23c932fdd62bee6451f
kanggongp <gongkang24@gmail.com>
2024-05-07 22:06:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React22
         </a>
       </header>
     </div>
