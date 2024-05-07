4767c25cbde59ecb1ca5c5bdccc4e2d7baa5435b
kanggongp <gongkang24@gmail.com>
2024-05-07 22:19:12 +0900

    click 01

--- src/router/Home/HomeContainer.js
+++ src/router/Home/HomeContainer.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 function HomeContainer(props) {
+
+  const click = () => {
+    console.log('clilck test1');
+  }
+
   return (
     <div>
       HomeContainer
+      <div>
+        test
+      </div>
+      <button onClick={click}>click</button>
     </div>
   );
 }
